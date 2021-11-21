02e998f047649d006c7a5bbf42cd1967d5dc9702
Baurice Nafack <bauricenafack@yahoo.fr>
2021-11-21 11:28:04 +0100

    non blocking working with 2 process, correctly received it rank

--- ring_topology/non_blocking_1.cpp
+++ ring_topology/non_blocking_1.cpp
@@ -2,7 +2,7 @@
 #include<mpi.h>
 
 int main(int argc,char *argv[]){
-	int numtasks,rank,next, prev, buf[2], itag_rank=1, itag_rec_prev=2, itag_rec_next=2;
+	int numtasks,rank,next, prev, buf[2]={100,100}, itag_rank=1, itag_rec_prev=2, itag_rec_next=2;
 	MPI_Request reqs[4]; // required variable for non-blocking calls
 	MPI_Status stats[4]; // required varaible  for Waitall routine
 
@@ -15,10 +15,11 @@ int main(int argc,char *argv[]){
 	prev = rank -1;
 	next = rank + 1;
 
-	int send = -rank;
+	int msgleft = -rank, msgright = rank;
+
 	if(rank==0) prev= numtasks-1;
 	if(rank== (numtasks-1)) next = 0;
-	int compt =0;
+	int cond=1000;
 	
 	itag_rank = rank*10;
 	itag_rec_prev= prev*10;
@@ -28,42 +29,47 @@ int main(int argc,char *argv[]){
 	if(rank %2==0){
 		
 
-		while(compt<3){
-		compt++;
-		MPI_Irecv(&buf[1],1,MPI_INT,next,itag_rec_next,MPI_COMM_WORLD,&reqs[0]);
+		while(cond !=rank){
+		MPI_Irecv(&buf[0],1,MPI_INT,next,itag_rec_next,MPI_COMM_WORLD,&reqs[0]); // buf[0] -> msgleft
 
-		MPI_Isend(&rank,1,MPI_INT,prev,itag_rank,MPI_COMM_WORLD,&reqs[1]);
+		MPI_Isend(&msgright,1,MPI_INT,prev,itag_rank,MPI_COMM_WORLD,&reqs[1]);
 		
 		// post non blocking receives and sends for neighbord
-		MPI_Irecv(&buf[0],1,MPI_INT,prev, itag_rec_prev,MPI_COMM_WORLD, &reqs[2]);
+		MPI_Irecv(&buf[1],1,MPI_INT,prev, itag_rec_prev,MPI_COMM_WORLD, &reqs[2]); //buf[1] -> msgright
 	
-		MPI_Isend(&send,1,MPI_INT,next,itag_rank,MPI_COMM_WORLD,&reqs[3]);
+		MPI_Isend(&msgleft,1,MPI_INT,next,itag_rank,MPI_COMM_WORLD,&reqs[3]);
 	
 		// wait for all non-blocking operations to complete
+		msgleft =  buf[0];
+		msgright = buf[1];
 
 		MPI_Waitall(4, reqs, stats);
-		std::cout<<" I received " <<  buf[0] << " and " << buf[1]<< ". My rank is " << rank  <<std::endl;
+	
+		std::cout<<" My msgright is " <<  msgright << " and my msgleft is " << msgleft << ". My rank is " << rank  <<std::endl;
+		cond =msgright;
 		}
 
 	}
 	else
 	{	
-		while(compt<3){
-		MPI_Isend(&rank,1,MPI_INT,prev,itag_rank,MPI_COMM_WORLD,&reqs[0]);
-		MPI_Irecv(&buf[1],1,MPI_INT,next,itag_rec_next,MPI_COMM_WORLD,&reqs[1]);
+		while( cond!= rank){
+		MPI_Isend(&msgleft,1,MPI_INT,prev,itag_rank,MPI_COMM_WORLD,&reqs[0]);
+		MPI_Irecv(&buf[0],1,MPI_INT,next,itag_rec_next,MPI_COMM_WORLD,&reqs[1]); // buf[0] -> msgleft
 
 			
 		// post non blocking receives and sends for neighbord
 	
 		
-		MPI_Irecv(&buf[0],1,MPI_INT,prev, itag_rec_prev,MPI_COMM_WORLD, &reqs[3]);
-		MPI_Isend(&send,1,MPI_INT,next,itag_rank,MPI_COMM_WORLD,&reqs[2]);
+		MPI_Irecv(&buf[1],1,MPI_INT,prev, itag_rec_prev,MPI_COMM_WORLD, &reqs[3]); // buef[1]-> msgright 
+		MPI_Isend(&msgright,1,MPI_INT,next,itag_rank,MPI_COMM_WORLD,&reqs[2]);
 		// wait for all non-blocking operations to complete
 
 		MPI_Waitall(4, reqs, stats);
+		msgleft=buf[0];
+		msgright= buf[1];
 		
-		std::cout<< " I received " << buf[0] << " and " << buf[1] << ". My rank is " << rank << std::endl;
-		compt= compt+1;
+		std::cout<< " My msgright is " << msgright << " and my msgleft is " << msgleft << ". My rank is " << rank << std::endl;
+		cond= msgright;
 		}
 	}
 	
